7eafd170be3adb0d50a505d495607c9d97c77edb
Gheorghe <madangheorghe.30@gmail.com>
2025-01-31 15:26:18 +0100

    create hobbies function

---
+++ hobbies.js
@@ -0,0 +1,11 @@
+const hobbyOne = 'Fotografia'
+const hobbyTwo = 'Gaming'
+const hobbyThree = 'Escurisionismo'
+console.log((trheeHobbies(hobbyOne, hobbyTwo, hobbyThree)));
+
+// funzione 
+function trheeHobbies (hobbyOne, hobbyTwo, hobbyThree){
+    const hobbiesArray = []
+    hobbiesArray.push(hobbyOne, hobbyTwo, hobbyThree)
+    return hobbiesArray
+}
