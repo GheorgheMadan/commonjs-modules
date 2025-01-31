9c525dc5e5fa49d1528ead53e24843f4623d4951
Gheorghe <madangheorghe.30@gmail.com>
2025-01-31 15:14:25 +0100

    create function names

---
+++ names.js
@@ -0,0 +1,11 @@
+
+const nome = 'Gheorghe'
+const cognome = 'Madan'
+
+console.log(firstLastName(nome, cognome));
+
+
+// funzione 
+function firstLastName (firstName, lastName){
+    return { 'first name': firstName, 'last name': lastName }
+}
